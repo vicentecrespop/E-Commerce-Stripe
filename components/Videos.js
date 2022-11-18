60a6a5eca45276b11138ec4a893adb51b0a6c504
vicentecrespop <vicentecrespo45@gmail.com>
2022-11-18 11:13:57 -0300

    Atualizei Videos

--- components/Videos.js
+++ components/Videos.js
@@ -6,37 +6,37 @@ export default function Videos() {
                 <div className="border border-transparent border-b-black pb-4 mb-4 uppercase text-[#555] font-extralight text-lg flex jusity-between w-2/3">
                     <span className="inline-block pr-2 w-1/6 font-bold">Oct 6 2022</span>
                     <span className="w-1/4">Vans</span>
-                    <span className="w-5/12">Kilian Roth 'Check'</span>
+                    <span className="w-5/12">Kilian Roth - Check</span>
                     <a href="https://www.youtube.com/watch?v=zXWHFCnOV0A" rel="noreferrer" target="_blank" className="max-w-1/5 bg-emerald-500 text-white font-bold rounded-md p-2 cursor-pointer">Assistir</a>
                 </div>
                 <div className="border border-transparent border-b-black pb-4 mb-4 uppercase text-[#555] font-extralight text-lg flex jusity-between w-2/3">
                     <span className="inline-block pr-2 w-1/6 font-bold">Jan 7 0215</span>
                     <span className="w-1/4">CultCrew</span>
-                    <span className="w-5/12">Chase Dehart 'Talk is Cheap'</span>
+                    <span className="w-5/12">Chase Dehart - Talk is Cheap</span>
                     <a href="https://www.youtube.com/watch?v=j3uhI1z94oU" rel="noreferrer" target="_blank" className="max-w-1/5 bg-emerald-500 text-white font-bold rounded-md p-2 cursor-pointer">Assistir</a>
                 </div>
                 <div className="border border-transparent border-b-black pb-4 mb-4 uppercase text-[#555] font-extralight text-lg flex jusity-between w-2/3">
                     <span className="inline-block pr-2 w-1/6 font-bold">Dez 24 2017</span>
                     <span className="w-1/4">United</span>
-                    <span className="w-5/12">Natham Williams</span>
+                    <span className="w-5/12">Natham Williams - Still United</span>
                     <a href="https://www.youtube.com/watch?v=nqMAoZiy0H8" rel="noreferrer" target="_blank" className="max-w-1/5 bg-emerald-500 text-white font-bold rounded-md p-2 cursor-pointer">Assistir</a>
                 </div>
                 <div className="border border-transparent border-b-black pb-4 mb-4 uppercase text-[#555] font-extralight text-lg flex jusity-between w-2/3">
                     <span className="inline-block pr-2 w-1/6 font-bold">Dez 3 2015</span>
                     <span className="w-1/4">Deadline</span>
-                    <span className="w-5/12">Garret Reynolds 'Deadline'</span>
+                    <span className="w-5/12">Garret Reynolds - Deadline</span>
                     <a href="https://www.youtube.com/watch?v=C5Ff2jpgK7o" rel="noreferrer" target="_blank" className="max-w-1/5 bg-emerald-500 text-white font-bold rounded-md p-2 cursor-pointer">Assistir</a>
                 </div>
                 <div className="border border-transparent border-b-black pb-4 mb-4 uppercase text-[#555] font-extralight text-lg flex jusity-between w-2/3">
                     <span className="inline-block pr-2 w-1/6 font-bold">Sept 15 2016</span>
                     <span className="w-1/4">Shadow Conspiracy</span>
-                    <span className="w-5/12">Simone Barraco 'What Could go Wrong'</span>
+                    <span className="w-5/12">Simone Barraco - What Could go Wrong</span>
                     <a href="https://www.youtube.com/watch?v=rCBMWvCct10" rel="noreferrer" target="_blank" className="max-w-1/5 bg-emerald-500 text-white font-bold rounded-md p-2 cursor-pointer">Assistir</a>
                 </div>
                 <div className="pb-4 mb-4 uppercase text-[#555] font-extralight text-lg flex jusity-between w-2/3">
                     <span className="inline-block pr-2 w-1/6 font-bold">Aug 12 2018</span>
                     <span className="w-1/4">BSD</span>
-                    <span className="w-5/12">Reed Stark 'BSD Transmission'</span>
+                    <span className="w-5/12">Reed Stark - BSD Transmission</span>
                     <a href="https://www.youtube.com/watch?v=l2unztfXxBc&t=200s" rel="noreferrer" target="_blank" className="max-w-1/5 bg-emerald-500 text-white font-bold rounded-md p-2 cursor-pointer">Assistir</a>
                 </div>
             </div>
